3de7f9105ad4b85fa9df2081ed5c0c0771979960
Tahsan <tahsanknmas14@gmail.com>
2023-02-14 18:35:52 +0600

    added news category

--- index.js
+++ index.js
@@ -3,17 +3,36 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-
 // middleware
 app.use(cors())
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews)
+    }
+})
+app.get('/news', (req, res) => {
+    res.send(news);
+});
+app.get('/news/:id', (req, res) => {
+    // console.log(req.params.id)
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
 })
 
 app.listen(port, () => {
